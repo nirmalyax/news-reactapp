07ce252cc87e099f9f21423aed58d44cdcefc93d
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2023-10-21 08:06:28 +0530

    @nirmalyax

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero-style.css'
 export default function Hero() {
   return (
-    <div className='hero '>Hero</div>
+    <div className='container p-3 '>Hero</div>
   )
 }
 
